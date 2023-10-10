c1bd736f95369b29b7b8ed63f9892d0b487fdd23
Austin <amartinez15@my.hpu.edu>
2023-10-10 05:53:53 +0000

    Added the new categories

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,16 @@
-class TasksController < ApplicationController
-  before_action :set_task, only: %i[ show edit update destroy ]
+# app/controllers/tasks_controller.rb
 
-  # GET /tasks or /tasks.json
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  # GET /tasks
+  # GET /tasks.json
   def index
     @tasks = Task.all
   end
 
-  # GET /tasks/1 or /tasks/1.json
+  # GET /tasks/1
+  # GET /tasks/1.json
   def show
   end
 
@@ -19,43 +23,47 @@ class TasksController < ApplicationController
   def edit
   end
 
-  # POST /tasks or /tasks.json
+  # POST /tasks
+  # POST /tasks.json
   def create
     @task = Task.new(task_params)
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to task_url(@task), notice: "Task was successfully created." }
+        format.html { redirect_to @task, notice: 'Task was successfully created.' }
         format.json { render :show, status: :created, location: @task }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new }
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /tasks/1 or /tasks/1.json
+  # PATCH/PUT /tasks/1
+  # PATCH/PUT /tasks/1.json
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to task_url(@task), notice: "Task was successfully updated." }
+        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
         format.json { render :show, status: :ok, location: @task }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :edit }
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /tasks/1 or /tasks/1.json
+  # DELETE /tasks/1
+  # DELETE /tasks/1.json
+  # In your TasksController
+  # app/controllers/tasks_controller.rb
   def destroy
+    @task = Task.find(params[:id])
     @task.destroy
-
-    respond_to do |format|
-      format.html { redirect_to tasks_url, notice: "Task was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to tasks_url, notice: 'Task was successfully deleted.'
   end
+
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -65,6 +73,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:name, :status, :order, :duedate, :location, :description)
+      params.require(:task).permit(:name, :status, :order, :duedate, :location, :description, :category, :completed)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_06_002832) do
+ActiveRecord::Schema.define(version: 2023_10_10_035219) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2023_10_06_002832) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
+    t.boolean "completed"
   end
 
 end
